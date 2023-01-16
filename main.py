7bb4440564f05b042e0889e10f150af3d7437038
Abdelaziz64 <aamin088@uottawa.ca>
2023-01-15 23:26:17 -0500

    Final commit

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
+import random
 
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B":4,
+    "C":6,
+    "D":8
+}
+
+def get_slot_machine_spin(rows,cols,symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) -1:
+                print(column[row], "|", end="|")
+            else:
+                print(columns[row], end = "")
 
 def deposit():
     while True:
@@ -46,7 +84,6 @@ def get_bet():
     return amount
 
 
-
 def main():
     balance = deposit()
     lines = get_numbers_of_lines()
@@ -66,5 +103,8 @@ def main():
     total_bet = bet*lines
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
     print(balance, lines)
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
